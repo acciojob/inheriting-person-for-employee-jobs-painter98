4a50a86e952d1d0183f37298685d64b0efe31f46
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-12 21:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // complete this js code
 function Person(name, age) {
-	greet(){
+	this.name=name;
+	this.age=age;
+	this.greet=()=>{
 		console.log(`Hello, my name is ${name}, I am ${age} years old.`);
 	}
 	
 }
 
 function Employee(name, age, jobTitle) extends Person {
-	jobGreet(){
+	this.name=name;
+	this.age=age;
+	this.jobTitle=jobTitle;
+	this.jobGreet=()=>{
 		console.log(`Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}`);
 	}
 }
